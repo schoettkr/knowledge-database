c816a82f55f0be29d61b864309ba0a03567e4108
LS8 <lennartschoettker@hotmail.com>
2018-10-31 17:46:19 +0100

    Add site url

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "schoettkr"  # (translatable)
 BLOG_TITLE = "Knowledge Database"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://schoettkr.github.io/knowledge-database/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
